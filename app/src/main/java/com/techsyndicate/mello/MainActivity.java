b3875bcb0ccff9e6e4735ce4b94f931696a1fcff
rachittt <guptarachit2004@gmail.com>
2019-07-03 19:30:01 +0530

    login wireframe

--- app/src/main/java/com/techsyndicate/mello/MainActivity.java
+++ app/src/main/java/com/techsyndicate/mello/MainActivity.java
@@ -2,12 +2,14 @@ package com.techsyndicate.mello;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
